3588bb0ee193ea8d67c8d9f5c4feb0cae2b28a48
Hussein <basel.hussein@accso.de>
2023-04-25 09:03:58 +0200

    adding bimsch as a package in the project

---
+++ packages/bimsch/src/index.js
@@ -0,0 +1,4 @@
+export * from "./lib/bimsch-data";
+export * from "./lib/bimsch-help";
+export * from "./lib/bimsch-helper";
+export * from "./lib/bimsch-map31";
--- packages/example-app/src/app/app.tsx
+++ packages/example-app/src/app/app.tsx
@@ -1,12 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-import { multi} from '@formular-js/core';
+import { ArrayClass, multi} from '@formular-js/core';
 import NxWelcome from './nx-welcome';
 
 export function App() {
+  const reverseArray = () => {
+    let array = new ArrayClass();
+    return array.multi([1,2,4], 3);
+  }
   return (
     <>
       <div>{multi(10, 10)}</div>
+      <div>{reverseArray()}</div>
       <NxWelcome title="example-app" />
 
       <div />
@@ -15,3 +20,7 @@ export function App() {
 }
 
 export default App;
+function reverseArray() {
+  throw new Error('Function not implemented.');
+}
+
